bab26c5168a3fb4b3a115b3d1d93b75288a82ec6
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-08-12 09:58:29 -0300

    Agora todas as classes estão respeitando o princípio da substituição de Liskov. Com as heranças respeitando esse princípio,conseguimos reaproveitar códigos.

--- app/src/main/java/com/lucascabral/solidprinciples/liskov/BaseEmployee.kt
+++ app/src/main/java/com/lucascabral/solidprinciples/liskov/BaseEmployee.kt
@@ -1,19 +1,13 @@
 package com.lucascabral.solidprinciples.liskov
 
-import com.lucascabral.solidprinciples.liskov.interfaces.ManagedInterface
+import com.lucascabral.solidprinciples.liskov.interfaces.EmployeeInterface
 
-open class Employee : BaseEmployee(), ManagedInterface {
-
+abstract class BaseEmployee: EmployeeInterface {
     override lateinit var firstName: String
     override lateinit var lastName: String
-    override lateinit var manager: Employee
     override var salary: Double = 0.0
 
     override fun calculateMonthlySalary(rank: Int) {
         this.salary = (12.5 + (rank * 2))*220
-    }
-
-    override fun assignManager(manager: Manager) {
-        this.manager = manager
     }
 }
